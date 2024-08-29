612cdfdc98e9af253ef97ab53a82ee20925f999a
Cristian Garcia <cgarcia@sproutloud.com>
2024-08-29 14:42:47 -0500

    fix viewpor

--- resources/js/components/ConfigForm.vue
+++ resources/js/components/ConfigForm.vue
@@ -48,7 +48,7 @@
                 </tbody>
             </table>
             <div class="flex items-center justify-end mt-4">
-                <button type="button" @click="addRow" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Agregar Fila</button>
+                <button type="button" @click="addRow" class="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Agregar Fila</button>
             </div>
 
             <div class="flex items-center justify-end mt-4">
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return redirect('/admin');
+
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/admin');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
