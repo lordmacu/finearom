e6e4e52b2d1c6caa5e2aa768e455dccd7f44c78f
anamariagr <anamariagarcia9709@gmail.com>
2024-09-03 12:32:47 -0500

    add btn export and import

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -9,8 +9,10 @@
         </x-admin.add-link>
     @endcan
 
-    <a href="/admin/products/export" class="btn btn-success">Exportar productos</a>
+    <div class="conten-btn">
+        <a href="/admin/products/export" title="Exportar Producto"><svg class="h-8 w-8 text-green-600"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />  <polyline points="15 3 21 3 21 9" />  <line x1="10" y1="14" x2="21" y2="3" /></svg></a>
 
+    </div>
     <div class="py-2">
         <form method="GET" action="{{ route('admin.product.index') }}">
             <div class="flex items-center space-x-2">
