38084bd5a3e0015c83d47cc6485b454406f9ba55
Cristian Garcia <cgarcia@sproutloud.com>
2024-09-06 10:14:40 -0500

    remove verify email

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
         <title>Laravel</title>
+        <link rel="icon" href="{{ asset('icon.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
